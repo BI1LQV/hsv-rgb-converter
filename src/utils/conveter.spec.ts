11960f2f13eb5f65c357250e7e5f327d6b387137
bi1lqv <bi1lqv.y@gmail.com>
2022-09-08 22:05:30 +0800

    test: 修复`rgb2hsv`测试错误

--- src/utils/conveter.spec.ts
+++ src/utils/conveter.spec.ts
@@ -2,25 +2,27 @@ import { describe, expect, it } from "vitest"
 import type { threeTuple } from "./converter"
 import { colorMap } from "./colorMap"
 import { HSV2RGB, RGB2HSV } from "./converter"
-// describe("rgb2hsv", () => {
-//   colorMap.forEach(({ rgb, hsv, name }) => {
-//     it(`convert ${name} rgb to correct hsv`, () => {
-//       const [h, s, v] = RGB2HSV(rgb.map(i => i / 255) as threeTuple)
-//       expect(h / 360).approximately(hsv[0], 0.001)
-//       expect(s).approximately(hsv[1], 0.001)
-//       expect(v).approximately(hsv[2], 0.001)
-//     })
-//   })
-// })
+
+const ALLOW_DELTA = 0.001
 
 describe("rgb2hsv", () => {
+  colorMap.forEach(({ rgb, hsv: [h0, s0, v0], name }) => {
+    it(`convert ${name} rgb to correct hsv`, () => {
+      const [h, s, v] = RGB2HSV(rgb.map(i => i / 255) as threeTuple)
+      expect(h / 360).approximately(h0, ALLOW_DELTA)
+      expect(s).approximately(s0, ALLOW_DELTA)
+      expect(v).approximately(v0, ALLOW_DELTA)
+    })
+  })
+})
+
+describe("hsv2rgb", () => {
   colorMap.forEach(({ rgb, hsv, name }) => {
     it(`convert ${name} hsv to correct rgb`, () => {
-      const newRGB = RGB2HSV([hsv[0] * 360, hsv[1], hsv[2]])
-      //   Array.from({ length: 3 }).forEach((_, i) => {
-      const i = 2
-      expect(newRGB[i]).approximately(rgb[i] / 255, 0.001)
-    //   })
+      const newRGB = HSV2RGB([hsv[0] * 360, hsv[1], hsv[2]])
+      Array.from({ length: 3 }).forEach((_, i) => {
+        expect(newRGB[i]).approximately(rgb[i] / 255, ALLOW_DELTA)
+      })
     })
   })
 })
