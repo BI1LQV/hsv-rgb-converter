72f917c2b7c62f9b88a4a608cc560d7eb0c433c2
BI1LQV <bi1lqv.y@gmail.com>
2022-09-12 16:18:53 +0800

    feat: 增加title

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,14 @@ import HSVSelector from "./components/HSVSelector.vue"
 </script>
 
 <template>
-  <div flex ma items-center>
-    <RGBSelector w-100 m-r-20></RGBSelector>
-    <ColorSelector size="large"></ColorSelector>
-    <HSVSelector w-90 m-l-20></HSVSelector>
+  <div flex ma items-center flex-col>
+    <div m-b-10 text-10 m-t--10>
+      HSV与RGB互转工具
+    </div>
+    <divflex flex ma items-center>
+      <RGBSelector w-90 m-r-20></RGBSelector>
+      <ColorSelector size="large"></ColorSelector>
+      <HSVSelector w-90 m-l-20></HSVSelector>
+    </divflex>
   </div>
 </template>
