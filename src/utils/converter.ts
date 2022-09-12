2e4b4dd54f40aaebaa40667905979c574b776cec
BI1LQV <bi1lqv.y@gmail.com>
2022-09-12 12:05:42 +0800

    refactor: 修改hsv rgb互转签名

--- src/utils/converter.ts
+++ src/utils/converter.ts
@@ -1,8 +1,7 @@
 // https://zhuanlan.zhihu.com/p/105886300
-export type threeTuple = [number, number, number]
-export type RGB = threeTuple
-export type HSV = threeTuple
-export function HSV2RGB([h, s, v]: HSV): RGB {
+export interface RGB { r: number; g: number; b: number }
+export interface HSV { h: number; s: number; v: number }
+export function HSV2RGB({ h, s, v }: HSV): RGB {
   /**
    * h 色调 在0-360度范围
    * s 饱和度 在0-1范围
@@ -11,26 +10,26 @@ export function HSV2RGB([h, s, v]: HSV): RGB {
   const c = v * s
   const x = c * (1 - Math.abs(h / 60 % 2 - 1))
   const m = v - c
-  let r1 = m
-  let g1 = m
-  let b1 = m
+  let r = m
+  let g = m
+  let b = m
   if (h < 60) {
-    r1 += c; g1 += x
+    r += c; g += x
   } else if (h < 120) {
-    r1 += x; g1 += c
+    r += x; g += c
   } else if (h < 180) {
-    g1 += c; b1 += x
+    g += c; b += x
   } else if (h < 240) {
-    g1 += x; b1 += c
+    g += x; b += c
   } else if (h < 300) {
-    r1 += x; b1 += c
+    r += x; b += c
   } else if (h < 360) {
-    r1 += c; b1 += x
+    r += c; b += x
   }
-  return [r1, g1, b1] as RGB
+  return { r, g, b }
 }
 
-export function RGB2HSV([r, g, b]: RGB): HSV {
+export function RGB2HSV({ r, g, b }: RGB): HSV {
   /**
    * rgb 都在0-1范围
    */
@@ -54,5 +53,5 @@ export function RGB2HSV([r, g, b]: RGB): HSV {
     s = delta / cMax
   }
 
-  return [h, s, cMax] as HSV
+  return { h, s, v: cMax }
 }
